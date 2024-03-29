35cf6a207df4ea70b3f493cdbf7a5c9e1db0cfba
meashishpatel <ashishpatel3009@gmail.com>
2024-03-29 14:33:33 +0530

    Fix bug in login functionality

--- Jarvis/config/Mineconfig.py
+++ Jarvis/config/Mineconfig.py
@@ -2,6 +2,6 @@
 email = "asyspatel@gmail.com"
 email_password = "twbs nwva eseu ebzh"
 wolframalpha_id = "LPHGG6-R4LV4RTUE7"
-
+f
 weather_api_key = "44ed293c3780699871007bbd13072abf"
 # Paste these in config.py
